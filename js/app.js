612abbfc7467584d6e0436e8783ea4716f7cd121
llonan <llonan@bk.ru>
2023-09-19 01:23:07 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+let $map = document.querySelector(".map")
+let mapContent, auditoriums, points
+
+$map.addEventListener("load", () => {
+	mapContent = $map.contentDocument
+	auditoriums = mapContent.getElementsByClassName('au')
+	points = mapContent.getElementsByClassName('point')
+	for (const elAuditorium of auditoriums) {
+		elAuditorium.addEventListener("click", function () {
+			for (const elErasingAuditorium of auditoriums) {
+				if (elErasingAuditorium !== this)
+					elErasingAuditorium.classList.remove("selected")
+			}
+			this.classList.toggle("selected")
+			let elPointCrassing
+			for (const elPoint of points) {
+				elPoint.classList.remove('selected-point')
+				if (elPoint.id.indexOf(elAuditorium.id) !== -1) {
+					elPointCrassing = elPoint
+				}
+			}
+			console.log(elPointCrassing)
+			if (elAuditorium.classList.contains('selected'))
+				elPointCrassing.classList.add('selected-point')
+		})
+		console.log(elAuditorium)
+	}
+})
+
