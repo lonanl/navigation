0fa1520e29063b0e89368ed2bcf02d1fb0a91955
llonan <llonan@bk.ru>
2023-09-23 20:52:38 +0300

    - добавлены кнопка и функция получения списка рёбер - выводит в консоль список ребер - теперь кнопки могут быть активными и неактивными, что уже применяется при трассировке, составлении списка ребер, во избежание ошибок

--- js/app.js
+++ js/app.js
@@ -1,50 +1,65 @@
-import {Graph, Vertex, Edge} from './graph.js'
+import {Graph} from './graph.js'
 
-let $map = document.querySelector(".map")
-let mapContent, auditoriums, points
+let $map = document.querySelector(".map") //объект отображения карты
+let mapContent, auditoriums, points //головной элемент документа карты, аудитории на карте, точки на карте
 
-$map.addEventListener("load", () => {
-	mapContent = $map.contentDocument
-	auditoriums = mapContent.getElementsByClassName('au')
-	points = mapContent.getElementsByClassName('point')
-	for (const elAuditorium of auditoriums) {
-		elAuditorium.addEventListener("click", function () {
-			for (const elErasingAuditorium of auditoriums) {
+$map.addEventListener("load", () => { //при загрузке карты
+
+	mapContent = $map.contentDocument //ищем контент
+	auditoriums = mapContent.getElementsByClassName('au') //ищем аудитории по классу (ПОМЕНЯТЬ НА ЦВЕТ)
+	points = mapContent.getElementsByClassName('point') //ищем точки на карте (ПОМЕНЯТЬ НА ЦВЕТ)
+
+	for (const elAuditorium of auditoriums) { //проходимся по аудиториям
+		elAuditorium.addEventListener("click", function () { //при нажатии на аудиторию
+
+			for (const elErasingAuditorium of auditoriums) { //стираем все аудитории
 				if (elErasingAuditorium !== this)
 					elErasingAuditorium.classList.remove("selected")
 			}
-			this.classList.toggle("selected")
-			let elPointCrassing
-			for (const elPoint of points) {
-				elPoint.classList.remove('selected-point')
-				if (elPoint.id.indexOf(elAuditorium.id) !== -1) {
-					elPointCrassing = elPoint
+			//сюда добавить занесение аудитории в выбранную
+
+			this.classList.toggle("selected") //стираем или закрашиваем аудиторию
+			let $elPointPainting //здесь будет закрашиваемая точка аудитории
+			for (const $elPoint of points) {
+				$elPoint.classList.remove('selected-point') //стираем все точки
+				if ($elPoint.id.indexOf(elAuditorium.id) !== -1) {
+					$elPointPainting = $elPoint
 				}
 			}
-			// console.log(elPointCrassing)
-			if (elAuditorium.classList.contains('selected'))
-				elPointCrassing.classList.add('selected-point')
+
+			if (elAuditorium.classList.contains('selected')) // закрашиваем или нет точку
+				$elPointPainting.classList.add('selected-point')
 		})
-		// console.log(elAuditorium)
+
 	}
 })
 
 let planName = "plan-01"
 
-export function change() {
-	if (planName === 'plan-01'){
+export function change() { //смена схемы на другой файл
+	if (planName === 'plan-01') {
 		$map.data = "plan-01-all.svg"
 		planName = "plan-01-all"
-	}
-	else {
+	} else {
 		$map.data = "plan-01.svg"
 		planName = "plan-01"
 	}
 }
 
-export function eraseTable() {
+export function eraseTable() { //стирание таблицы и восстановление плана
 	console.log(Graph.$tableContent.childElementCount)
-	while(Graph.$tableContent.firstChild)
+	while (Graph.$tableContent.firstChild)
 		Graph.$tableContent.firstChild.remove()
-		Graph.$objectGraph.data = "plan-01-graph.svg"
+	Graph.$objectGraph.data = "plan-01-graph.svg"
+	activateButton('tracing')
+	deactivateButton('erase')
+	deactivateButton('get-list-of-edges')
+}
+
+export function activateButton(buttonClass) {
+	document.getElementsByClassName(buttonClass)[0].classList.remove('non-active-button')
+
+}
+export function deactivateButton(buttonClass) {
+	document.getElementsByClassName(buttonClass)[0].classList.add('non-active-button')
 }
--- js/graph.js
+++ js/graph.js
@@ -1,3 +1,5 @@
+import {activateButton, deactivateButton} from "./app.js";
+
 export class Graph {
 	static $graphSvg
 	static $objectGraph;
@@ -5,7 +7,6 @@ export class Graph {
 
 	static tracing() {
 		Graph.$objectGraph = document.querySelector('.graph');
-
 
 		function erase($el) {
 			$el.setAttribute('stroke-width', '3')
@@ -19,32 +20,42 @@ export class Graph {
 
 			coordinates = coordinates.substring(firstSpace + 1)
 
-			let secondSpace = coordinates.indexOf(' ')
-			let lineType = ''
+			// let secondSpace = coordinates.indexOf(' ')
+			// let lineType = ''
 			let secondLetterPosition;
 
+
 			if (coordinates.indexOf('H') !== -1) {
-				lineType = 'horizontal'
+				// lineType = 'horizontal'
 				secondLetterPosition = coordinates.indexOf('H')
 				y1 = coordinates.substring(0, secondLetterPosition)
 				y2 = y1
-				x2 = coordinates.substring(secondLetterPosition+1)
+				x2 = coordinates.substring(secondLetterPosition + 1)
 			} else if (coordinates.indexOf('V') !== -1) {
-				lineType = 'vertical'
+				// lineType = 'vertical'
 				secondLetterPosition = coordinates.indexOf('V')
 				y1 = coordinates.substring(0, secondLetterPosition)
 				x2 = x1
-				y2 = coordinates.substring(secondLetterPosition+1)
+				y2 = coordinates.substring(secondLetterPosition + 1)
+			}
+
+			if (x1 >= x2 && y1 >= y2) {
+				let t = x2
+				x2 = x1
+				x1 = t
+				t = y2
+				y2 = y1
+				y1 = t
 			}
 
 			function addTdToTR(value, cellClass, tr) {
-				let td =  document.createElement('td')
+				let td = document.createElement('td')
 				td.innerHTML = value
-				// td.setAttribute('class', String(cellClass))
+				td.setAttribute('class', String(cellClass))
 				tr.appendChild(td)
 			}
 
-			let weight = Number((((x2-x1)**2+(y2-y1)**2)**0.5).toFixed(2))
+			let weight = Number((((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5).toFixed(2))
 
 			addTdToTR($el.getAttribute('id'), 'id', row)
 			addTdToTR(x1, 'x1', row)
@@ -54,37 +65,52 @@ export class Graph {
 			addTdToTR(weight, 'weight', row)
 			addTdToTR('same-floor', 'type', row)
 
-
-
-
 			Graph.$tableContent.appendChild(row)
 		}
 
 		// Graph.$objectGraph.addEventListener("load", () => {
-			Graph.$graphSvg = Graph.$objectGraph.contentDocument.getElementsByTagName('svg')[0]
-			let timeout = 0
-			for (const $graphEl of Graph.$graphSvg.getElementsByTagName('path')) {
-				if ($graphEl.getAttribute('stroke') === '#FF5F5F') {
-					console.log($graphEl)
-					setTimeout(erase, timeout, $graphEl)
-					timeout += 70
-				}
+		Graph.$graphSvg = Graph.$objectGraph.contentDocument.getElementsByTagName('svg')[0]
+		let timeout = 0
+		for (const $graphEl of Graph.$graphSvg.getElementsByTagName('path')) {
+			if ($graphEl.getAttribute('stroke') === '#FF5F5F') {
+				console.log($graphEl)
+				setTimeout(erase, timeout, $graphEl)
+				timeout += 70
 			}
-		// })
+		}
+		deactivateButton('tracing')
+		activateButton('erase')
+		activateButton('get-list-of-edges')
+	}
+
+	static getListOfEdges() {
+		let rows = Graph.$tableContent.getElementsByTagName('tr')
+		let listOfEdges = []
+		for (const $row of rows) {
+			let id = $row.getElementsByClassName('id')[0].innerText
+			let x1 = Number($row.getElementsByClassName('x1')[0].innerText)
+			let y1 = Number($row.getElementsByClassName('y1')[0].innerText)
+			let x2 = Number($row.getElementsByClassName('y2')[0].innerText)
+			let y2 = Number($row.getElementsByClassName('y2')[0].innerText)
+			let weight = Number($row.getElementsByClassName('weight')[0].innerText)
+			let type = $row.getElementsByClassName('type')[0].innerText
+			listOfEdges.push(new Edge(id, new Vertex(x1, y1), new Vertex(x2, y2), weight, type))
+		}
+		console.log(listOfEdges)
 	}
 }
 
 export class Vertex {
-	constructor(x,y, adjacentPoints = []) {
+	constructor(x, y, adjacentPoints = []) {
 		this.x = x
 		this.y = y
 		this.adjacentPoints = adjacentPoints
 	}
-
 }
 
 export class Edge {
-	constructor(vertex1, vertex2, weight = 0, type = '') {
+	constructor(id = '', vertex1, vertex2, weight = 0, type = '') {
+		this.id = id
 		this.vertex1 = vertex1
 		this.vertex2 = vertex2
 		this.weight = weight
